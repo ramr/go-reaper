1e876c55ba0509ae556fa5e698ba4b74e6e26599
Andrey Klimentyev <andrey.klimentyev@flant.com>
2018-10-01 11:13:42 +0300

    Added subreaper functionality

--- reaper.go
+++ reaper.go
@@ -7,15 +7,18 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+
+	"golang.org/x/sys/unix"
 )
 
 // Reaper configuration.
 type Config struct {
-	Pid              int
-	Options          int
-	DisablePid1Check bool
-	StatusChannel    chan Status
-	Debug            bool
+	Pid                  int
+	Options              int
+	DisablePid1Check     bool
+	EnableChildSubreaper bool
+	StatusChannel        chan Status
+	Debug                bool
 }
 
 // Reaped child process status information.
@@ -138,9 +141,10 @@ func Reap() {
 	 *  is to reap all processes.
 	 */
 	Start(Config{
-		Pid:              -1,
-		Options:          0,
-		DisablePid1Check: false,
+		Pid:                  -1,
+		Options:              0,
+		DisablePid1Check:     false,
+		EnableChildSubreaper: false,
 	})
 
 } /*  End of [exported] function  Reap.  */
@@ -160,10 +164,17 @@ func Start(config Config) {
 	if !config.DisablePid1Check {
 		mypid := os.Getpid()
 		if 1 != mypid {
-			if config.Debug {
-				fmt.Printf(" - Grim reaper disabled, pid not 1\n")
+			if config.EnableChildSubreaper {
+				fmt.Printf(" - Not pid 1, enabling subreaper\n")
+				err := unix.Prctl(unix.PR_SET_CHILD_SUBREAPER, 1, 0, 0, 0)
+				if err != nil {
+					fmt.Printf(" - Failed to enable subreaper: %s\n", err)
+					return
+				}
+			} else {
+				fmt.Printf(" - Grim reaper disabled, pid not 1, subreaper disabled\n")
+				return
 			}
-			return
 		}
 	}
 
